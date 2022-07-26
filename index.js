21c71f54a2c4469edea43324a2e3ddd106a8207a
Daria Approvato <dariaapprovato@gmail.com>
2022-07-26 17:59:09 -0400

    Completed Array Filter Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+   return drivers.filter(function(driver) {
+    return string.toLowerCase() === driver.toLowerCase()
+   })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (driver){
+        return driver.startsWith(string)
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.name === string
+    })
+}
